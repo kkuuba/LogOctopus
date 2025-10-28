ef32baf83284f82694947c6427323f2b989c5d22
Meerchant <kkuuba.k1@gmail.com>
2025-10-28 23:47:17 +0100

    added local logs collection + fix issues with sudo cmds

--- device_watchdog.py
+++ device_watchdog.py
@@ -1,4 +1,5 @@
 from fabric import Connection
+from invoke import Context
 import pandas as pd
 from datetime import datetime
 from dateutil import parser
@@ -6,40 +7,88 @@ import re
 import threading
 
 class DeviceWatchdog:
+    """
+    A class used to logs collection for target device using defined configuration.
+    """
     def __init__(self, device_config):
+        """
+        Initializes a DeviceWatchdog instance.
+
+        Args:
+            device_config (dict): Connection and logging configuraiton for target device.
+        """
         self.device_config = device_config
+        self.local_device = device_config["local_device"]
         self.device_name = device_config["device_name"]
-        self.ssh_connection = Connection(
-            host=device_config["ip_address"],
-            user=device_config["user"],
-            port=device_config["port"],
-            connect_kwargs={"password": device_config["password"]})
+        if self.local_device:
+            self.ssh_connection = Context()
+        else:
+            self.ssh_connection = Connection(
+                host=device_config["ip_address"],
+                user=device_config["user"],
+                port=device_config["port"],
+                connect_kwargs={"password": device_config["password"]})
         self.collected_data = {}
         self.errors = pd.DataFrame({"time": [], "error_info": []})
         self.collection_ongoing = False
         self.thread = None
 
-    def execute_remote_cmd(self, cmd):
+    def execute_cmd(self, cmd):
+        """
+        Execute command via SSH on target device with failure handling.
+
+        Args:
+            cmd (str): Target cmd string.
+
+        Returns:
+            str: Full cmd output if execution was successful.
+        """
         try:
-            self.ssh_connection.open()
-            cmd_result = self.ssh_connection.run(cmd)
+            root_requried = True if "sudo " in cmd else False
+            if self.local_device:
+                if root_requried:
+                    cmd_result = self.ssh_connection.sudo(cmd, password=self.device_config["password"], hide=True, pty=True)
+                else:
+                    cmd_result = self.ssh_connection.local(cmd, hide=True, pty=True)
+            else:
+                self.ssh_connection.open()
+                if root_requried:
+                    cmd_result = self.ssh_connection.sudo(cmd, password=self.device_config["password"], hide=True, pty=True)
+                else:
+                    cmd_result = self.ssh_connection.run(cmd, hide=True, pty=True)
             if cmd_result.ok:
                 return cmd_result.stdout
-            error_entry = {"time": datetime.now(), "error_info": f"cmd '{cmd}' failed with -> {cmd_result.stderr.strip()}"}
+            error_entry = {"time": [datetime.now()], "error_info": [f"cmd '{cmd}' failed with -> {cmd_result.stderr.strip()}"]}
             self.errors = pd.concat([self.errors, pd.DataFrame(error_entry)], ignore_index=True)
         except Exception as e:
-            error_entry = {"time": datetime.now(), "error_info": f"cmd '{cmd}' failed with -> {e}"}
+            error_entry = {"time": [datetime.now()], "error_info": [f"cmd '{cmd}' failed with -> {e}"]}
             self.errors = pd.concat([self.errors, pd.DataFrame(error_entry)], ignore_index=True)
         return None
 
     def initialize_log_collectors(self):
+        """
+        Initialzie log collectors for all defined log file configs.
+        """
         for log_config in self.device_config["log_file_configs"]:
-            self.ssh_connection.run(log_config["log_activation_cmd"])
+            self.execute_cmd(log_config["log_activation_cmd"])
             self.collected_data[log_config["log_name"]] = pd.DataFrame({"time": [], "content": []})
 
     def get_log_file_content(self, log_config):
+        """
+        Extract log content to dictionary with timestamps and logs entries. All data processing is 
+        executed based on data in provided log configuration.
+
+        Args:
+            log_config (dict): Log collector configuration data.
+
+        Returns:
+            dict: All extracted log entries with timestamps.
+        """
         log_content = {"time": [], "content": []}
-        for entry_line in self.execute_remote_cmd(log_config["log_file_cmd"]).split("\n"):
+        raw_log_content = self.execute_cmd(log_config["log_file_cmd"])
+        if raw_log_content is None:
+            return log_content
+        for entry_line in self.execute_cmd(log_config["log_file_cmd"]).split("\n"):
             entry_line_match = re.search(log_config["data_extraction_regex"], entry_line)
             if entry_line_match:
                 log_content["time"].append(parser.parse(entry_line_match.group("TIME")))
@@ -48,20 +97,35 @@ class DeviceWatchdog:
         return log_content
 
     def get_all_log_files_content(self):
+        """
+        Extract all logs based on logs configs and save them to target pandas dataframes.
+        """
         for log_config in self.device_config["log_file_configs"]:
             new_log_content = pd.DataFrame(self.get_log_file_content(log_config))
             self.collected_data[log_config["log_name"]] = pd.concat([self.collected_data[log_config["log_name"]], new_log_content], ignore_index=True).drop_duplicates(subset="time", keep="first")
 
     def start_logs_collection(self):
+        """
+        Start logs collection background thread.
+        """
         self.collection_ongoing = True
         self.thread = threading.Thread(target=self.logs_collection_loop, args=(self.device_config["collection_interval"],))
         self.thread.start()
 
     def stop_logs_collection(self):
+        """
+        Stop logs collection background thread.
+        """
         self.collection_ongoing = False
         self.thread.join()
 
     def logs_collection_loop(self, interval):
+        """
+        Main background loop for logs collection from target device.
+
+        Args:
+            interval (int): Interval between logs extraction from target device.
+        """
         collection_stop_event = threading.Event()
         while self.collection_ongoing:
             collection_stop_event.wait(timeout=interval)
--- tests/test_device_watchdog.py
+++ tests/test_device_watchdog.py
@@ -6,9 +6,10 @@ with open("config/device_config.json", "r") as file:
     example_config = json.load(file)
 
 device_config = example_config["device_configs"][0]
-example_device_wd = DeviceWatchdog(device_config=device_config)
+local_device_config = example_config["device_configs"][1]
 
 def test_normal_log_collection_scenario():
+    example_device_wd = DeviceWatchdog(device_config=device_config)
     example_device_wd.initialize_log_collectors()
     example_device_wd.start_logs_collection()
     sleep(30)
@@ -16,7 +17,16 @@ def test_normal_log_collection_scenario():
     assert len(example_device_wd.collected_data["syslog"]) > 0
     assert len(example_device_wd.errors) == 0
 
+def test_local_log_collection_scenario():
+    example_device_wd = DeviceWatchdog(device_config=local_device_config)
+    example_device_wd.initialize_log_collectors()
+    example_device_wd.start_logs_collection()
+    sleep(30)
+    example_device_wd.stop_logs_collection()
+    assert len(example_device_wd.collected_data["syslog"]) > 0
+
 def test_broken_connection_scenario():
+    example_device_wd = DeviceWatchdog(device_config=device_config)
     example_device_wd.ssh_connection.host = "incorrect_ip"
     example_device_wd.initialize_log_collectors()
     example_device_wd.start_logs_collection()
@@ -26,6 +36,7 @@ def test_broken_connection_scenario():
     assert len(example_device_wd.collected_data["syslog"]) == 0
 
 def test_incorrect_config_scenario():
+    example_device_wd = DeviceWatchdog(device_config=device_config)
     example_device_wd.device_config["log_file_configs"][0]["log_file_cmd"] = "cat /incorrect_log_path/log.txt"
     example_device_wd.device_config["log_file_configs"][1]["log_file_cmd"] = "cat /incorrect_log_path/log.txt"
     example_device_wd.initialize_log_collectors()
@@ -33,4 +44,5 @@ def test_incorrect_config_scenario():
     sleep(30)
     example_device_wd.stop_logs_collection()
     assert len(example_device_wd.errors) > 0
-    assert len(example_device_wd.collected_data["syslog"]) > 0
+    assert len(example_device_wd.collected_data["syslog"]) == 0
+    assert len(example_device_wd.collected_data["kernel"]) == 0
